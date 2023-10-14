97f5160428baae91c8cfd3b66b2e687fc2acd604
ony789 <rawan02600@gmail.com>
2023-10-14 05:50:37 -0700

    o

--- _printf.c
+++ _printf.c
@@ -11,14 +11,25 @@ int _printf(const char *format, ...)
 	int i, count = 0, printed_chars = 0;
 	
 	va_list list;
+	char buffer;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(list, format)
 		
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_puffer(buffer, &buff_ind);
+			write(1, &format[i], 1);
+			printed_chars++;
+		}
+
+
 
 
 }
