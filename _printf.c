c8a2bdfd554ab3e50f61b7a5216d3a6190f0da99
ony789 <rawan02600@gmail.com>
2023-10-14 04:49:00 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -8,6 +8,17 @@
  */
 int _printf(const char *format, ...)
 {
+	int i, count = 0, printed_chars = 0;
+	
+	va_list list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format)
+		
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
 
 
 }
