5346028cf88b7370692abfc432030434c2cef19a
Bero-spero <zoro234564@gmail.com>
2023-10-14 13:38:17 -0700

    bero

--- _printf.c
+++ _printf.c
@@ -8,36 +8,29 @@
  */
 int _printf(const char *format, ...)
 {
-        int i, count = 0, printed_chars = 0;
+	int i, count = 0, printed_chars = 0;
 
-        va_list list;
-        char new_buffer;
+	va_list list;
+	char new_buffer;
 	int new_buff_ind = 0;
 
-        if (format == NULL)
-                return (-1);
+	if (format == NULL)
+		return (-1);
 
-	va_start(list, format)
-		
+	va_start(list, format);
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			new_buffer[new_buff_ind++] = format[i];
 			if (new_buff_ind == BUFF_SIZE)
-				print_puffer(new_buffer, &new_buff_ind);
+				print_buffer(new_buffer, &new_buff_ind);
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(new_buffer, &new_buff_ind);
-	int new_flags, new_width, new_precision, new_size;
-			new_flags = get_new_flags(new_format, &i);
-			new_width = get_new_width(new_format, &i, new_list);
-			new_precision = get_new_precision(new_format, &i, new_list);
-			new_size = get_new_size(new_format, &i);
-			++i;
-			count = handle_new_print(format, &i, list, new_buffer, new_flags, new_width, new_precision, new_size);
 			if (count == -1)
 				return (-1);
 			printed_chars += count;
@@ -47,12 +40,13 @@ int _printf(const char *format, ...)
 	va_end(list);
 	return (printed_chars);
 }
-void print_buffer(char new_buffer[], int *new_buff_ind);
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @new_buffer: Array of chars
  * @new_buff_ind: Index at which to add next char, represents the length.
  */
+void print_buffer(char *new_buffer, int *new_buff_ind)
 {
 	if (*new_buff_ind > 0)
 		write(1, &new_buffer[0], *new_buff_ind);
